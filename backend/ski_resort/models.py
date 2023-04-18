66ef3afbae6539e8148f99ef3999fb0b75001991
bdavis83 <bdavis83@gmail.com>
2023-04-17 22:21:03 -0400

    created SkiResortSerializer

--- backend/ski_resort/models.py
+++ backend/ski_resort/models.py
@@ -4,6 +4,6 @@ class SkiResort (models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
-    latitude = models.CharField(max_length=255)
-    longitude = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=4)
+    longitude = models.DecimalField(max_digits=4)
     favorite = models.ForeignKey()
