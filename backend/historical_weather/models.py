5f382e4c8baecd71e6deebfa824722e915a96883
bdavis83 <bdavis83@gmail.com>
2023-04-17 21:26:06 -0400

    created weather_forecast model

--- backend/historical_weather/models.py
+++ backend/historical_weather/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from ski_resort.models import SkiResort
 
 class HistoricalWeather (models.Model):
-    location = models.ForeignKey(SkiResort)
+    location = models.ForeignKey(SkiResort, on_delete=models.CASCADE)
     date = models.DateField()
     high_temp = models.CharField(max_length=255)
     low_temp = models.CharField(max_length=255)
--- backend/weather_forecast/models.py
+++ backend/weather_forecast/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from ski_resort.models import SkiResort
 
-# Create your models here.
+class WeatherForecast (models.Model):
+    location = models.ForeignKey(SkiResort, on_delete=models.CASCADE)
+    condition = models.CharField(max_length=255)
+    high_temp = models.CharField(max_length=255)
+    low_temp = models.CharField(max_length=255)
+    wind = models.CharField(max_length=255)
+    precipitation = models.CharField(max_length=255)
+    forecast_time = models.TimeField()
+    
